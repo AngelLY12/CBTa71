2aa61d4bba0ee956c4b4d5a7aa0edfab6ff19f05
Angel <lopezyanezangell@gmail.com>
2025-10-02 13:28:00 -0600

    feat(Webhook) Add webhook configuration

--- backend/school-management/config/services.php
+++ backend/school-management/config/services.php
@@ -36,7 +36,8 @@ return [
     ],
     'stripe' =>[
         'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET')
+        'secret' => env('STRIPE_SECRET'),
+        'webhook'=>env('STRIPE_WEBHOOK_SECRET')
 
     ],
 
