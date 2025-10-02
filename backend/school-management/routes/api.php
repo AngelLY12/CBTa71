2e9f352e341bbb161f17f07768506757e958816c
Angel <lopezyanezangell@gmail.com>
2025-10-02 13:29:13 -0600

    Feat(routes): Add routes to student controllers

--- backend/school-management/routes/api.php
+++ backend/school-management/routes/api.php
@@ -1,8 +1,32 @@
 <?php
 
+use App\Http\Controllers\Students\DashboardController;
+use App\Http\Controllers\Students\CardsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Students\WebhookController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->middleware(['auth:sanctum'])->group(function (){
+    Route::prefix('dashboard')->middleware('role:student')->group(function (){
+        Route::get('/',[DashboardController::class,'index']);
+        Route::get('/pending',[DashboardController::class,'pending']);
+        Route::get('/paid',[DashboardController::class,'paid']);
+        Route::get('/overdue',[DashboardController::class,'overdue']);
+        Route::get('/history',[DashboardController::class,'history']);
+
+    });
+    Route::prefix('cards')->middleware('role:student')->group(function(){
+        Route::get('/',[CardsController::class,'index']);
+        Route::post('/',[CardsController::class,'store']);
+        Route::post('/setup-intent',[CardsController::class,'setupIntent']);
+        Route::delete('/{paymentMethodId}',[CardsController::class,'destroy']);
+    });
+    Route::post('/stripe/webhook', [WebhookController::class, 'handle']);
+
+});
+
+
