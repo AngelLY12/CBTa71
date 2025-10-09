520f1810d0f781516f9e45ab2249a0b0a5a60d52
Angel <lopezyanezangell@gmail.com>
2025-10-09 13:54:26 -0600

    feat: Roles and permissions were added to the seed

--- backend/school-management/database/seeders/DatabaseSeeder.php
+++ backend/school-management/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+               // Limpiar cache de permisos
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // ------------------------
+        // PERMISOS DE STUDENT
+        // ------------------------
+        $studentPermissions = [
+            'view own financial overview',
+            'view own pending concepts summary',
+            'view own paid concepts summary',
+            'view own overdue concepts summary',
+            'view payments history',
+            'view cards',
+            'create setup',
+            'create and view card',
+            'delete card',
+            'view payment history',
+            'view pending concepts',
+            'create payment',
+            'view overdue concepts',
+        ];
+
+        foreach ($studentPermissions as $perm) {
+            Permission::firstOrCreate(['name' => $perm]);
+        }
+
+        // ------------------------
+        // PERMISOS DE FINANCIAL STAFF
+        // ------------------------
+        $staffPermissions = [
+            'view all financial overview',
+            'view all pending concepts summary',
+            'view all students summary',
+            'view all paid concepts summary',
+            'view concepts history',
+            'view concepts',
+            'create concepts',
+            'update concepts',
+            'finalize concepts',
+            'disable concepts',
+            'view debts',
+            'validate debt',
+            'view payments',
+            'view students',
+        ];
+
+        foreach ($staffPermissions as $perm) {
+            Permission::firstOrCreate(['name' => $perm]);
+        }
+
+        // ------------------------
+        // CREAR ROLES Y ASIGNAR PERMISOS
+        // ------------------------
+        $studentRole = Role::firstOrCreate(['name' => 'student']);
+        $studentRole->syncPermissions($studentPermissions);
+
+        $staffRole = Role::firstOrCreate(['name' => 'financial staff']);
+        $staffRole->syncPermissions($staffPermissions);
     }
+
 }
