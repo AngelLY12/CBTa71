4c26ef11238a8fde9ce3664ac2d731eac5e53c19
Angel <lopezyanezangell@gmail.com>
2025-10-18 17:36:01 -0600

    refactor: ReconcilePayments now is distpach instead of daily job

--- backend/school-management/app/Console/Kernel.php
+++ backend/school-management/app/Console/Kernel.php
@@ -10,7 +10,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('concepts:dispatch-finalize-job')->daily();
-         $schedule->job(new ReconcilePayments)->dailyAt('01:00');
 
     }
 
