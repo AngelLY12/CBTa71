d03903c1fadb8d53c978ef775587bf7958f76388
Angel <lopezyanezangell@gmail.com>
2025-10-26 23:14:51 -0600

    feat: Add generic ValidationException

---
+++ backend/school-management/app/Exceptions/ValidationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exceptions;
+
+class ValidationException extends DomainException
+{
+    public function __construct(string $message, int $code = 422)
+    {
+        parent::__construct($message, $code);
+    }
+}
+
