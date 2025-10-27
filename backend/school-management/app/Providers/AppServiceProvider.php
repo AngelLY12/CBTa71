be32e8c7a10bc3b5ec5fc53cb4d3dc802ba55296
Angel <lopezyanezangell@gmail.com>
2025-10-26 23:26:14 -0600

    feat: Add interface registration and deployment to repositories

--- backend/school-management/app/Providers/AppServiceProvider.php
+++ backend/school-management/app/Providers/AppServiceProvider.php
@@ -2,6 +2,26 @@
 
 namespace App\Providers;
 
+use App\Core\Domain\Repositories\Command\CareerRepInterface;
+use App\Core\Domain\Repositories\Command\Payments\PaymentConceptRepInterface;
+use App\Core\Domain\Repositories\Command\Payments\PaymentMethodRepInterface;
+use App\Core\Domain\Repositories\Command\Payments\PaymentRepInterface;
+use App\Core\Domain\Repositories\Command\Stripe\StripeGatewayInterface;
+use App\Core\Domain\Repositories\Command\StudentDetailReInterface;
+use App\Core\Domain\Repositories\Command\UserRepInterface;
+use App\Core\Domain\Repositories\Query\Payments\PaymentConceptQueryRepInterface;
+use App\Core\Domain\Repositories\Query\Payments\PaymentQueryRepInterface;
+use App\Core\Domain\Repositories\Query\UserQueryRepInterface;
+use App\Core\Infraestructure\Repositories\Command\EloquentCareerRepository;
+use App\Core\Infraestructure\Repositories\Command\EloquentStudentDetailRepository;
+use App\Core\Infraestructure\Repositories\Command\EloquentUserRepository;
+use App\Core\Infraestructure\Repositories\Command\Payments\EloquentPaymentConceptRepository;
+use App\Core\Infraestructure\Repositories\Command\Payments\EloquentPaymentMethodRepository;
+use App\Core\Infraestructure\Repositories\Command\Payments\EloquentPaymentRepository;
+use App\Core\Infraestructure\Repositories\Command\Stripe\StripeGateway;
+use App\Core\Infraestructure\Repositories\Query\EloquentUserQueryRepository;
+use App\Core\Infraestructure\Repositories\Query\Payments\EloquentPaymentConceptQueryRepository;
+use App\Core\Infraestructure\Repositories\Query\Payments\EloquentPaymentQueryRepository;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +32,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(StripeGatewayInterface::class, StripeGateway::class);
+        $this->app->bind(PaymentMethodRepInterface::class, EloquentPaymentMethodRepository::class);
+        $this->app->bind(PaymentRepInterface::class, EloquentPaymentRepository::class);
+        $this->app->bind(PaymentQueryRepInterface::class, EloquentPaymentQueryRepository::class);
+        $this->app->bind(PaymentConceptRepInterface::class, EloquentPaymentConceptRepository::class);
+        $this->app->bind(PaymentConceptQueryRepInterface::class, EloquentPaymentConceptQueryRepository::class);
+        $this->app->bind(UserRepInterface::class, EloquentUserRepository::class);
+        $this->app->bind(UserQueryRepInterface::class, EloquentUserQueryRepository::class);
+        $this->app->bind(CareerRepInterface::class, EloquentCareerRepository::class);
+        $this->app->bind(StudentDetailReInterface::class,EloquentStudentDetailRepository::class);
+
     }
 
     /**
