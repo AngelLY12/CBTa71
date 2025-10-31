ccd61386fc5be8c88318e58719255885e3163dc2
Angel <lopezyanezangell@gmail.com>
2025-10-31 10:53:51 -0600

    feat: Add secureheaders to protect accept headers

--- backend/school-management/bootstrap/app.php
+++ backend/school-management/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SecureHeadersMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+        $middleware->append(SecureHeadersMiddleware::class);
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
