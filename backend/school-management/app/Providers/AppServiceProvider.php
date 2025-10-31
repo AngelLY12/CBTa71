09ee8eed8a4a56803b5356b0f320f2b6b10e4904
Angel <lopezyanezangell@gmail.com>
2025-10-31 10:51:02 -0600

    feat: Add new repo, service and ratelimiter global

--- backend/school-management/app/Providers/AppServiceProvider.php
+++ backend/school-management/app/Providers/AppServiceProvider.php
@@ -6,13 +6,16 @@ use App\Core\Domain\Repositories\Command\CareerRepInterface;
 use App\Core\Domain\Repositories\Command\Payments\PaymentConceptRepInterface;
 use App\Core\Domain\Repositories\Command\Payments\PaymentMethodRepInterface;
 use App\Core\Domain\Repositories\Command\Payments\PaymentRepInterface;
+use App\Core\Domain\Repositories\Command\RefreshTokenRepInterface;
 use App\Core\Domain\Repositories\Command\Stripe\StripeGatewayInterface;
 use App\Core\Domain\Repositories\Command\StudentDetailReInterface;
 use App\Core\Domain\Repositories\Command\UserRepInterface;
 use App\Core\Domain\Repositories\Query\Payments\PaymentConceptQueryRepInterface;
 use App\Core\Domain\Repositories\Query\Payments\PaymentQueryRepInterface;
 use App\Core\Domain\Repositories\Query\UserQueryRepInterface;
+use App\Core\Infraestructure\Cache\CacheService;
 use App\Core\Infraestructure\Repositories\Command\EloquentCareerRepository;
+use App\Core\Infraestructure\Repositories\Command\EloquentRefreshTokenRepository;
 use App\Core\Infraestructure\Repositories\Command\EloquentStudentDetailRepository;
 use App\Core\Infraestructure\Repositories\Command\EloquentUserRepository;
 use App\Core\Infraestructure\Repositories\Command\Payments\EloquentPaymentConceptRepository;
@@ -23,6 +26,8 @@ use App\Core\Infraestructure\Repositories\Query\EloquentUserQueryRepository;
 use App\Core\Infraestructure\Repositories\Query\Payments\EloquentPaymentConceptQueryRepository;
 use App\Core\Infraestructure\Repositories\Query\Payments\EloquentPaymentQueryRepository;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -42,6 +47,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(UserQueryRepInterface::class, EloquentUserQueryRepository::class);
         $this->app->bind(CareerRepInterface::class, EloquentCareerRepository::class);
         $this->app->bind(StudentDetailReInterface::class,EloquentStudentDetailRepository::class);
+        $this->app->bind(RefreshTokenRepInterface::class,EloquentRefreshTokenRepository::class);
+        $this->app->singleton(CacheService::class, function () {return new CacheService();});
 
     }
 
@@ -50,6 +57,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('global', function ($request) {
+            return Limit::perMinute(30)->by(
+                optional($request->user())->id ?: $request->ip()
+            );
+        });
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
