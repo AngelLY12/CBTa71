f8541174577cbc1da94f0d215f744e66a9ceb4b5
Angel <lopezyanezangell@gmail.com>
2025-10-31 10:54:35 -0600

    feat: Add sanctum limit to token and cors configuration

--- backend/school-management/config/sanctum.php
+++ backend/school-management/config/sanctum.php
@@ -21,6 +21,7 @@ return [
         Sanctum::currentApplicationUrlWithPort(),
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
+    'expiration' => 15,
 
     /*
     |--------------------------------------------------------------------------
