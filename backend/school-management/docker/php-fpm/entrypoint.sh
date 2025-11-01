cd3617dcc9c57b992898908e1cfaeb3990956e47
Angel <lopezyanezangell@gmail.com>
2025-11-01 14:05:01 -0600

    refactor: php-fpm

--- backend/school-management/docker/php-fpm/entrypoint.sh
+++ backend/school-management/docker/php-fpm/entrypoint.sh
@@ -18,8 +18,6 @@ echo "Puerto de Laravel: ${PORT:-80}"
 
 echo "Laravel preparado. Iniciando servicios..."
 
-# Iniciar PHP-FPM en background
-php-fpm -D
 
 # Iniciar Nginx en foreground (Railway necesita un proceso principal)
-exec nginx -g "daemon off;"
+exec php-fpm
