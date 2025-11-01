3f13845250323c805eca5867075f118faf4a6751
Angel <lopezyanezangell@gmail.com>
2025-11-01 17:47:03 -0600

    fix: Fix sleep time in entrypoint

--- backend/school-management/docker/php-fpm/entrypoint.sh
+++ backend/school-management/docker/php-fpm/entrypoint.sh
@@ -15,8 +15,6 @@ php artisan migrate --force || { echo "Error al migrar"; exit 1; }
 echo "Ejecutando seeders..."
 php artisan db:seed --force || { echo "Error al ejecutar seeders"; exit 1; }
 
-sleep 5
-
 echo "Laravel preparado. Iniciando servicios..."
 
 exec php-fpm
