6ba65fb910cc9976e333ae34b318664b99ae8ae8
Angel <lopezyanezangell@gmail.com>
2025-11-01 22:42:22 -0600

    refactor: Add sleept time after check laravel status

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-LARAVEL_HOST="cbta71.railway.internal"
-LARAVEL_PORT=9000
+LARAVEL_URL="http://cbta71.railway.internal:9000/api/up" # puedes crear un endpoint /health que devuelva 200
 MAX_ATTEMPTS=20
 SLEEP_TIME=10
 
 echo "Esperando a que Laravel esté listo..."
 
 for i in $(seq 1 $MAX_ATTEMPTS); do
-  if nc -z "$LARAVEL_HOST" "$LARAVEL_PORT"; then
-    echo "Laravel PHP-FPM está listo (intento $i/$MAX_ATTEMPTS)"
+  HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$LARAVEL_URL" || echo 000)
+  
+  if [ "$HTTP_CODE" -eq 200 ]; then
+    echo "Laravel está listo (intento $i/$MAX_ATTEMPTS)"
     break
   fi
 
-  echo "Esperando a Laravel... (intento $i/$MAX_ATTEMPTS)"
+  echo "Laravel aún no responde ($HTTP_CODE) (intento $i/$MAX_ATTEMPTS)"
   sleep $SLEEP_TIME
 
   if [ "$i" -eq $MAX_ATTEMPTS ]; then
