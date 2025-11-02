56d7158680e4a23b1460662a5ef7681bde8e1865
Angel <lopezyanezangell@gmail.com>
2025-11-02 17:43:24 -0600

    empty

--- backend/school-management/routes/web.php
+++ backend/school-management/routes/web.php
@@ -6,4 +6,9 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/api/documentation', function () {
+    return redirect('/api/documentation');
+});
+
+
 require __DIR__.'/auth.php';
