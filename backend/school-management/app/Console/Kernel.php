5286e4d63d16377a8d91ce852ebb3c4f7fcfe800
Angel <lopezyanezangell@gmail.com>
2025-11-04 14:31:36 -0600

    feat: Add schedule to new jobs

--- backend/school-management/app/Console/Kernel.php
+++ backend/school-management/app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use App\Jobs\CleanDeleteConceptsJob;
+use App\Jobs\CleanDeleteUsersJob;
+use App\Jobs\CleanExpiredTokensJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\ReconcilePayments;
@@ -10,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('concepts:dispatch-finalize-job')->daily();
+         $schedule->job(new CleanExpiredTokensJob())->days([0, 3])->at('03:00');
+         $schedule->job(new CleanDeleteUsersJob())->weekly()->at('00:00');
+         $schedule->job(new CleanDeleteConceptsJob())->weekly()->at('00:00');
 
     }
 
