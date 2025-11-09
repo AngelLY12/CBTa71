c2923ea1deaaa43751968f83e79d2a8968770207
Angel <lopezyanezangell@gmail.com>
2025-11-08 18:45:24 -0600

    feat: Add commands to dispatch clean jobs

--- backend/school-management/app/Console/Kernel.php
+++ backend/school-management/app/Console/Kernel.php
@@ -2,20 +2,17 @@
 
 namespace App\Console;
 
-use App\Jobs\CleanDeleteConceptsJob;
-use App\Jobs\CleanDeleteUsersJob;
-use App\Jobs\CleanExpiredTokensJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Jobs\ReconcilePayments;
+
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('concepts:dispatch-finalize-job')->daily();
-         $schedule->job(new CleanExpiredTokensJob())->days([0, 3])->at('03:00');
-         $schedule->job(new CleanDeleteUsersJob())->weekly()->at('00:00');
-         $schedule->job(new CleanDeleteConceptsJob())->weekly()->at('00:00');
+         $schedule->command('tokens:dispatch-clean-expired-tokens')->days([0, 3])->at('03:00');
+         $schedule->command('users:dispatch-delete-users')->weekly()->at('00:00');
+         $schedule->command('concepts:dispatch-delete-concepts')->weekly()->at('00:00');
 
     }
 
