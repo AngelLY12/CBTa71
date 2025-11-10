0c32ec4afa832cbd23b4650ea3b76f5e5c1c04ca
Angel <lopezyanezangell@gmail.com>
2025-11-09 23:02:26 -0600

    fix:little change

--- backend/school-management/app/Core/Domain/Repositories/Query/Payments/PaymentConceptQueryRepInterface.php
+++ backend/school-management/app/Core/Domain/Repositories/Query/Payments/PaymentConceptQueryRepInterface.php
@@ -3,9 +3,9 @@
 namespace App\Core\Domain\Repositories\Query\Payments;
 
 use App\Core\Application\DTO\Response\PaymentConcept\PendingSummaryResponse;
-use App\Core\Domain\Entities\PaymentConcept;
 use App\Core\Domain\Entities\User;
 use Illuminate\Pagination\LengthAwarePaginator;
+use App\Core\Domain\Entities\PaymentConcept;
 
 interface PaymentConceptQueryRepInterface{
     //Dashboard Student
@@ -20,5 +20,4 @@ interface PaymentConceptQueryRepInterface{
     public function getOverduePayments(User $user):array;
     public function getPendingWithDetailsForStudents(array $userIds): array;
     public function finalizePaymentConcepts(): void;
-
 }
