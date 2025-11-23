511eb09ffb7b1ed652dae022d93343af13463543
Angel <lopezyanezangell@gmail.com>
2025-11-23 17:26:37 -0600

    fix: Fix middleware params

--- backend/school-management/routes/api.php
+++ backend/school-management/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Exceptions\Test\TestDomainException;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CareerController;
 use App\Http\Controllers\FindEntityController;
@@ -47,17 +46,17 @@ Route::prefix('v1')->middleware(['auth:sanctum'])->group(function (){
 
     });
     Route::prefix('cards')->middleware('role:student')->group(function(){
-        Route::middleware('permission:view cards', 'throttle:global')->get('/',[CardsController::class,'index']);
-        Route::middleware('permission:create setup', 'throttle:10,1')->post('/',[CardsController::class,'store']);
-        Route::middleware('permission:delete card', 'throttle:10,1')->delete('/{paymentMethodId}',[CardsController::class,'destroy']);
+        Route::middleware(['permission:view cards', 'throttle:global'])->get('/',[CardsController::class,'index']);
+        Route::middleware(['permission:create setup', 'throttle:10,1'])->post('/',[CardsController::class,'store']);
+        Route::middleware(['permission:delete card', 'throttle:10,1'])->delete('/{paymentMethodId}',[CardsController::class,'destroy']);
     });
     Route::prefix('history')->middleware(['role:student', 'throttle:global'])->group(function(){
         Route::middleware('permission:view payment history')->get('/',[PaymentHistoryController::class,'index']);
     });
     Route::prefix('pending-payment')->middleware('role:student')->group(function(){
-        Route::middleware('permission:view pending concepts', 'throttle:global')->get('/',[PendingPaymentController::class,'index']);
-        Route::middleware('permission:create payment','throttle:5,1')->post('/',[PendingPaymentController::class,'store']);
-        Route::middleware('permission:view overdue concepts', 'throttle:global')->get('/overdue',[PendingPaymentController::class,'overdue']);
+        Route::middleware(['permission:view pending concepts', 'throttle:global'])->get('/',[PendingPaymentController::class,'index']);
+        Route::middleware(['permission:create payment','throttle:5,1'])->post('/',[PendingPaymentController::class,'store']);
+        Route::middleware(['permission:view overdue concepts', 'throttle:global'])->get('/overdue',[PendingPaymentController::class,'overdue']);
 
     });
 
@@ -70,21 +69,21 @@ Route::prefix('v1')->middleware(['auth:sanctum'])->group(function (){
         Route::middleware('permission:refresh all dashboard')->post('/refresh',[StaffDashboardController::class,'refreshDashboard']);
     });
     Route::prefix('concepts')->middleware('role:financial staff')->group(function(){
-        Route::middleware('permission:view concepts', 'throttle:global')->get('/', [ConceptsController::class, 'index']);
-        Route::middleware('permission:create concepts', 'throttle:10,1')->post('/', [ConceptsController::class, 'store']);
-        Route::middleware('permission:update concepts', 'throttle:10,1')->put('/{concept}', [ConceptsController::class, 'update']);
-        Route::middleware('permission:update concepts', 'throttle:10,1')->patch('/{concept}', [ConceptsController::class, 'update']);
-        Route::middleware('permission:finalize concepts', 'throttle:10,1')->post('/{concept}/finalize', [ConceptsController::class, 'finalize']);
-        Route::middleware('permission:disable concepts', 'throttle:10,1')->post('/{concept}/disable', [ConceptsController::class, 'disable']);
-        Route::middleware('permission:eliminate concepts', 'throttle:10,1')->delete('/{concept}/eliminate', [ConceptsController::class, 'eliminate']);
-        Route::middleware('permission:eliminate logical concept', 'throttle:10,1')->post('/{concept}/eliminateLogical',[ConceptsController::class,'eliminateLogical']);
-        Route::middleware('permission:activate concept', 'throttle:10,1')->post('/{concept}/activate',[ConceptsController::class,'activate']);
+        Route::middleware(['permission:view concepts', 'throttle:global'])->get('/', [ConceptsController::class, 'index']);
+        Route::middleware(['permission:create concepts', 'throttle:10,1'])->post('/', [ConceptsController::class, 'store']);
+        Route::middleware(['permission:update concepts', 'throttle:10,1'])->put('/{concept}', [ConceptsController::class, 'update']);
+        Route::middleware(['permission:update concepts', 'throttle:10,1'])->patch('/{concept}', [ConceptsController::class, 'update']);
+        Route::middleware(['permission:finalize concepts', 'throttle:10,1'])->post('/{concept}/finalize', [ConceptsController::class, 'finalize']);
+        Route::middleware(['permission:disable concepts', 'throttle:10,1'])->post('/{concept}/disable', [ConceptsController::class, 'disable']);
+        Route::middleware(['permission:eliminate concepts', 'throttle:10,1'])->delete('/{concept}/eliminate', [ConceptsController::class, 'eliminate']);
+        Route::middleware(['permission:eliminate logical concept', 'throttle:10,1'])->post('/{concept}/eliminateLogical',[ConceptsController::class,'eliminateLogical']);
+        Route::middleware(['permission:activate concept', 'throttle:10,1'])->post('/{concept}/activate',[ConceptsController::class,'activate']);
     });
 
     Route::prefix('debts')->middleware('role:financial staff')->group(function(){
-        Route::middleware('permission:view debts', 'throttle:global')->get('/', [DebtsController::class, 'index']);
-        Route::middleware('permission:validate debt', 'throttle:10,1')->post('/validate', [DebtsController::class, 'validatePayment']);
-        Route::middleware('permission:view stripe-payments', 'throttle:10,1')->get('/stripe-payments', [DebtsController::class, 'getStripePayments']);
+        Route::middleware(['permission:view debts', 'throttle:global'])->get('/', [DebtsController::class, 'index']);
+        Route::middleware(['permission:validate debt', 'throttle:10,1'])->post('/validate', [DebtsController::class, 'validatePayment']);
+        Route::middleware(['permission:view stripe-payments', 'throttle:10,1'])->get('/stripe-payments', [DebtsController::class, 'getStripePayments']);
     });
 
     Route::prefix('payments')->middleware(['role:financial staff', 'throttle:global'])->group(function(){
