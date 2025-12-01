7bd8bacac5ad859e4667ed3d8f5c2d38ce93dce1
Angel <lopezyanezangell@gmail.com>
2025-12-01 16:56:09 -0600

    fix: Fix namespace's libraries

--- backend/school-management/app/Core/Application/UseCases/Payments/Staff/Debts/GetPaymentsFromStripeUseCase.php
+++ backend/school-management/app/Core/Application/UseCases/Payments/Staff/Debts/GetPaymentsFromStripeUseCase.php
@@ -4,7 +4,7 @@ namespace App\Core\Application\UseCases\Payments\Staff\Debts;
 
 use App\Core\Application\Mappers\GeneralMapper;
 use App\Core\Domain\Repositories\Command\Stripe\StripeGatewayInterface;
-use App\Core\Domain\Repositories\Query\UserQueryRepInterface;
+use App\Core\Domain\Repositories\Query\User\UserQueryRepInterface;
 use App\Exceptions\ValidationException;
 
 class GetPaymentsFromStripeUseCase
