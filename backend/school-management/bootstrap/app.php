282994f315949133c2a01f094d9b69a11a97016f
Angel <lopezyanezangell@gmail.com>
2025-11-30 21:58:07 -0600

    feat: Add new commands and new Response macro in withExceptions

--- backend/school-management/bootstrap/app.php
+++ backend/school-management/bootstrap/app.php
@@ -9,6 +9,8 @@ use Illuminate\Database\QueryException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Spatie\Permission\Exceptions\UnauthorizedException;
 use App\Exceptions\DomainException;
+use App\Http\Middleware\CheckUserStatus;
+use App\Http\Middleware\LogUserAction;
 use App\Http\Middleware\SecureHeadersMiddleware;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
@@ -16,6 +18,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -43,13 +46,26 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'log.action' => LogUserAction::class,
+            'user.status' => CheckUserStatus::class
         ]);
 
         //
     })
     ->withSchedule(function (Schedule $schedule){
         $schedule->command('backup:dispatch-create-backup-job')->dailyAt('02:00');
+        $schedule->command('app:dispatch-promote-students-job')
+        ->dailyAt('23:55')
+        ->when(function () {
+            $today = now();
+            $lastDay = $today->endOfMonth()->day;
+            $daysBeforeEnd = $lastDay - $today->day;
+            return $daysBeforeEnd <= 8
+                && in_array($today->month, config('promotions.allowed_months'));
+        });
+        $schedule->command('cache:dispatch-clean-cache-job')->cron('0 0 1 */3 *'); 
         $schedule->command('backup:clean')->dailyAt('02:30');
+        $schedule->command('logs:dispatch-clean-older-logs-job')->quarterly();
         $schedule->command('db:auto-restore')->dailyAt('03:00');
         $schedule->command('concepts:dispatch-finalize-job')->daily();
         $schedule->command('tokens:dispatch-clean-expired-tokens')->everyFourHours();
@@ -59,100 +75,57 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('invites:dispatch-clean-expired-invites-job')->weekly()->at('02:00');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        
         $exceptions->render(function (DomainException $e, Request $request) {
             logger('Handler ejecutado con: ' . get_class($e));
-
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage(),
-            ], $e->getStatusCode());
+            return Response::error($e->getMessage(), $e->getStatusCode());
         });
 
         $exceptions->render(function (AuthenticationException $e, Request $request) {
-        return response()->json([
-            'success' => false,
-            'message' => 'No estás autenticado',
-            ], 401);
+            return Response::error('No estás autenticado', 401);
         });
 
         $exceptions->render(function ($e, Request $request) {
             if ($e instanceof AuthorizationException || $e instanceof UnauthorizedException) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'No tienes permisos para realizar esta acción.',
-                ], 403);
+                return Response::error('No tienes permisos para realizar esta acción.', 403);
             }
         });
 
         $exceptions->render(function (QueryException $e, Request $request) {
             logger()->error('Database error: ' . $e->getMessage());
             if ($e->errorInfo[1] === 1062) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Registro duplicado, ya existe un usuario con esos datos.',
-                    'errors' => $e->errorInfo,
-                ], 409);
+                return Response::error('Registro duplicado, ya existe una entidad con esos datos.', 409, $e->errorInfo);
             }
-            return response()->json([
-                'success' => false,
-                'message' => 'Error interno al procesar la base de datos.',
-                'errors' => $e->errorInfo
-            ], 500);
+            return Response::error('Error interno al procesar la base de datos.', 500, $e->errorInfo);
         });
 
         $exceptions->render(function (ModelNotFoundException $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Recurso no encontrado',
-            ], 404);
+            return Response::error('Recurso no encontrado', 404);
         });
 
         $exceptions->render(function (\InvalidArgumentException $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage(),
-            ], 422);
+            return Response::error($e->getMessage(), 422);
         });
 
         $exceptions->render(function (CardException $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage(),
-            ], 422);
+            return Response::error($e->getMessage(), 422);
         });
 
         $exceptions->render(function (RateLimitException $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Demasiadas solicitudes a Stripe, intenta más tarde.',
-            ], 429);
+            return Response::error('Demasiadas solicitudes a Stripe, intenta más tarde.', 429);
         });
 
         $exceptions->render(function (ApiErrorException $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Error al comunicarse con Stripe, intenta más tarde.',
-            ], 502);
+            return Response::error('Error al comunicarse con Stripe, intenta más tarde.', 502);
         });
 
         $exceptions->render(function (ValidationException $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Errores de validación',
-                'errors' => $e->errors(),
-            ], 400);
+            return Response::error('Errores de validación', 400, $e->errors());
         });
-
 
         $exceptions->render(function (\Throwable $e, Request $request) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Ocurrió un error inesperado',
-                'error' => $e->getMessage(),
-            ], 500);
+            return Response::error('Ocurrió un error inesperado', 500, [$e->getMessage()]);
         });
-
         $exceptions->shouldRenderJsonWhen(function (Request $request, $e) {
             return $request->is('api/*') || $request->expectsJson();
         });
