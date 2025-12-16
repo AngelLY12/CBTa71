a75abe1c9570b57baa5799f3d929f654d4618bd3
Angel <lopezyanezangell@gmail.com>
2025-12-16 10:30:06 -0600

    feat: Add legend to notify the user when his payment is underpaid or overpaid

--- backend/school-management/app/Mail/PaymentValidatedMail.php
+++ backend/school-management/app/Mail/PaymentValidatedMail.php
@@ -3,6 +3,7 @@
 namespace App\Mail;
 
 use App\Core\Application\DTO\Request\Mail\PaymentValidatedEmailDTO;
+use App\Core\Domain\Enum\Payment\PaymentStatus;
 use Illuminate\Bus\Queueable;
 use MailerSend\Helpers\Builder\Personalization;
 use Illuminate\Mail\Mailable;
@@ -30,15 +31,17 @@ class PaymentValidatedMail extends Mailable
         $voucherNumber = $this->data->payment_method_detail['oxxo']['number'] ?? 'No aplica';
         $speiReference = $this->data->payment_method_detail['spei']['reference'] ?? 'No aplica';
         $type_payment_method = $this->data->payment_method_detail['type'] ?? 'Desconocido';
-
+        $paymentLegend = $this->buildPaymentLegend();
         $messageDetails = "
             <p><strong>Concepto:</strong> {$this->data->concept_name}</p>
             <p><strong>Monto:</strong> $".number_format($this->data->amount, 2)."</p>
+            <p><strong>Monto recibido: $".number_format($this->data->amount_received, 2)." </strong></p>
             <p><strong>Método de pago:</strong> {$type_payment_method}</p>
             <p><strong>Código de referencia:</strong> {$this->data->payment_intent_id}</p>
             <p><strong>Voucher OXXO:</strong> {$voucherNumber}</p>
             <p><strong>Referencia SPEI:</strong> {$speiReference}</p>
-            <p><strong>URL comprobante:</strong> <a href='{$this->data->url}' target='_blank'>{$this->data['url']}</a></p>
+            <p><strong>URL comprobante:</strong> <a href='{$this->data->url}' target='_blank'>{$this->data->url}</a></p>
+            {$paymentLegend}
         ";
 
         $personalization = [
@@ -61,5 +64,38 @@ class PaymentValidatedMail extends Mailable
         throw $e;
     }
     }
+
+    private function buildPaymentLegend(): string
+    {
+        return match ($this->data->status) {
+            PaymentStatus::UNDERPAID->value =>
+                "<p style='color:#d97706;'>
+                Detectamos que el monto recibido es menor al esperado.
+                <br>
+                <strong>Monto pendiente:</strong> $"
+                . number_format(
+                    $this->data->amount - $this->data->amount_received,
+                    2
+                ) .
+                "</p>",
+
+            PaymentStatus::OVERPAID->value =>
+                "<p style='color:#059669;'>
+                Tu pago tiene un <strong>monto extra</strong>.
+                <br>
+                <strong>Saldo extra pagado:</strong> $"
+                . number_format(
+                    $this->data->amount_received - $this->data->amount,
+                    2
+                ) .
+                "</p>",
+
+            PaymentStatus::SUCCEEDED->value => '',
+
+            default =>
+            '',
+        };
+    }
+
 
 }
