7cbb2f6da8592c0d40e21b71e73db94de591dc7a
Angel <lopezyanezangell@gmail.com>
2025-12-21 16:59:31 -0600

    feat: Add metadata field to include more information

--- backend/school-management/app/Core/Application/DTO/Response/User/UserChangedStatusResponse.php
+++ backend/school-management/app/Core/Application/DTO/Response/User/UserChangedStatusResponse.php
@@ -7,17 +7,7 @@ namespace App\Core\Application\DTO\Response\User;
  *     schema="UserChangedStatusResponse",
  *     type="object",
  *     description="Respuesta después de cambiar el estado de uno o más usuarios",
- *     @OA\Property(
- *         property="updatedUsers",
- *         type="array",
- *         description="Usuarios que han cambiado su estado",
- *         @OA\Items(
- *             type="object",
- *             @OA\Property(property="name", type="string", example="Juan Pérez"),
- *             @OA\Property(property="curp", type="string", example="PEPJ800101HDFRRN09"),
- *             @OA\Property(property="status", type="string", example="activo")
- *         )
- *     ),
+ *
  *     @OA\Property(
  *         property="newStatus",
  *         type="string",
@@ -35,7 +25,6 @@ namespace App\Core\Application\DTO\Response\User;
 class UserChangedStatusResponse
 {
     public function __construct(
-        public readonly ?array $updatedUsers,
         public readonly ?string $newStatus,
         public readonly int $totalUpdated
     )
--- backend/school-management/app/Core/Application/DTO/Response/User/UserWithUpdatedPermissionsResponse.php
+++ backend/school-management/app/Core/Application/DTO/Response/User/UserWithUpdatedPermissionsResponse.php
@@ -18,11 +18,23 @@ namespace App\Core\Application\DTO\Response\User;
  *         example={"find user", "create payment"}
  *     ),
  *     @OA\Property(
- *         property="totalUpdated",
- *         type="integer",
- *         description="Número total de usuarios afectados por la actualización",
- *         example=10
- *     )
+ *           property="metadata",
+ *           type="object",
+ *           description="Datos extra sobre la operación",
+ *           @OA\Property(property="totalFound", type="integer",  description="Número total de usuarios encontrados", example=30),
+ *           @OA\Property(property="totalUpdated", type="integer",  description="Número total de usuarios afectados", example=20),
+ *           @OA\Property(property="failed", type="integer", description="Número total de usuarios no afectados por fallo", example=10),
+ *           @OA\Property(property="failedUsers", type="array", description="Lista de ID de usuarios fallidos", @OA\Items(type="string"),
+ *              example={1,2,3,4}),
+ *           @OA\Property(
+ *              property="operations",
+ *              type="object",
+ *              description="Operaciones realizadas",
+ *              @OA\Property(property="permissions_removed", type="array", @OA\Items(type="string"), example={"view"}),
+ *              @OA\Property(property="permissions_added", type="array", @OA\Items(type="string"), example={"create"}),
+ *              @OA\Property(property="roles_processed", type="integer", example=5)
+ *          ),
+ *     ),
  * )
  */
 class UserWithUpdatedPermissionsResponse
@@ -32,7 +44,7 @@ class UserWithUpdatedPermissionsResponse
         public readonly ?string $curp,
         public readonly ?string $role=null,
         public readonly array $updatedPermissions,
-        public readonly int $totalUpdated
+        public readonly ?array $metadata
     )
     {
     }
