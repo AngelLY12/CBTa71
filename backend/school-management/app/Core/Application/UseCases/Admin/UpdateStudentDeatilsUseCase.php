973561ca85ca85c6f08a4e9d835c3d53cea924c1
Angel <lopezyanezangell@gmail.com>
2025-12-21 17:41:40 -0600

    fix: Fix file name

---
+++ backend/school-management/app/Core/Application/UseCases/Admin/UpdateStudentDeatilsUseCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Core\Application\UseCases\Admin;
+
+use App\Core\Domain\Entities\User;
+use App\Core\Domain\Repositories\Command\User\StudentDetailReInterface;
+use App\Jobs\ClearStaffCacheJob;
+
+class UpdateStudentDeatilsUseCase
+{
+    public function __construct(
+        private StudentDetailReInterface $repo
+    )
+    {
+    }
+
+    public function execute(int $userId, array $fields): User
+    {
+        $update=$this->repo->updateStudentDetails($userId, $fields);
+        ClearStaffCacheJob::dispatch()->delay(now()->addSeconds(rand(1, 10)));
+        return $update;
+    }
+}
