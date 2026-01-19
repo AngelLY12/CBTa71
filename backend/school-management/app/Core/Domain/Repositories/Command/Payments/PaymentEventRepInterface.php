4b785fe37e05a4898dd0937551380fa68aa5c422
Angel <lopezyanezangell@gmail.com>
2026-01-19 13:47:13 -0600

    feat: Add command and query interfaces to PaymentEvent

---
+++ backend/school-management/app/Core/Domain/Repositories/Command/Payments/PaymentEventRepInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Core\Domain\Repositories\Command\Payments;
+
+use App\Core\Domain\Entities\PaymentEvent;
+
+interface PaymentEventRepInterface
+{
+    public function create(PaymentEvent $event): PaymentEvent;
+    public function update(int $paymentEventId, array $fields): PaymentEvent;
+}
