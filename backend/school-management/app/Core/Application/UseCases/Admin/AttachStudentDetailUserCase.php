c57a61790ff90fd8fd630a614e7fd359b8f1d219
Angel <lopezyanezangell@gmail.com>
2026-01-21 16:15:35 -0600

    fix: Fix imports

--- backend/school-management/app/Core/Application/UseCases/Admin/AttachStudentDetailUserCase.php
+++ backend/school-management/app/Core/Application/UseCases/Admin/AttachStudentDetailUserCase.php
@@ -4,8 +4,8 @@ namespace App\Core\Application\UseCases\Admin;
 
 use App\Core\Application\DTO\Request\StudentDetail\CreateStudentDetailDTO;
 use App\Core\Domain\Entities\User;
-use App\Core\Domain\Repositories\Command\StudentDetailReInterface;
-use App\Core\Domain\Repositories\Query\UserQueryRepInterface;
+use App\Core\Domain\Repositories\Command\User\StudentDetailReInterface;
+use App\Core\Domain\Repositories\Query\User\UserQueryRepInterface;
 use App\Exceptions\Conflict\UserAlreadyHaveStudentDetailException;
 use App\Jobs\ClearStaffCacheJob;
 
