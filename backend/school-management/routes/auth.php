b57ac1b5f6e7f7550cf52909bc1b6e4bb5aef0eb
Angel <lopezyanezangell@gmail.com>
2026-01-21 11:11:01 -0600

    refactor: Comment register and login endpoints

--- backend/school-management/routes/auth.php
+++ backend/school-management/routes/auth.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-
+/*
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
     ->name('register');
@@ -15,7 +15,7 @@ Route::post('/register', [RegisteredUserController::class, 'store'])
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware('guest')
     ->name('login');
-
+*/
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->middleware('guest')
     ->name('password.email');
