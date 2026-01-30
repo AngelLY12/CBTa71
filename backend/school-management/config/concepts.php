c62bffd53b98d087e9c7696c08aacb447f542bae
Angel <lopezyanezangell@gmail.com>
2026-01-30 10:35:24 -0600

    refactor: Add default values to config, allowing env var

--- backend/school-management/config/concepts.php
+++ backend/school-management/config/concepts.php
@@ -4,11 +4,11 @@ use App\Core\Domain\Enum\User\UserRoles;
 
 return [
     'amount' => [
-        'min' => 10,
-        'max' => 25000,
+        'min' => '10.00',
+        'max' => env('AMOUNT_MAX', '25000.00'),
         'notifications' => [
-            'enabled' => true,
-            'threshold' => '2500',
+            'enabled' => (bool) env('AMOUNT_NOTIFY_ENABLED', true),
+            'threshold' => env('AMOUNT_NOTIFY_THRESHOLD', '2500.00'),
             'recipient_roles' => UserRoles::administrationRoles(),
             'channels' => ['mail'],
             'mail' => [
