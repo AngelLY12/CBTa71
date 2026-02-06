bf2a056169fd2b4a7bbac9c6146c957111bce1d1
Angel <lopezyanezangell@gmail.com>
2026-02-06 09:23:09 -0600

    feat: Add new fields to identify users

--- backend/school-management/app/Core/Application/DTO/Response/PaymentConcept/ConceptNameAndAmountResponse.php
+++ backend/school-management/app/Core/Application/DTO/Response/PaymentConcept/ConceptNameAndAmountResponse.php
@@ -6,7 +6,7 @@ namespace App\Core\Application\DTO\Response\PaymentConcept;
  * @OA\Schema(
  *     schema="ConceptNameAndAmountResponse",
  *     type="object",
- *     @OA\Property(property="userId", type="string", nullable=true, description="ID del usuario asociado al pago", example=34"),
+ *     @OA\Property(property="userId", type="string", nullable=true, description="ID del usuario asociado al pago", example=34),
  *     @OA\Property(property="user_name", type="string", nullable=true, description="Nombre del usuario asociado al pago", example="Juan Pérez"),
  *     @OA\Property(property="n_control", type="string", nullable=true, description="Número de control del usuario asociado al pago", example="97763726"),
  *     @OA\Property(property="concept_name", type="string", nullable=true, description="Nombre del concepto de pago", example="Pago de inscripción"),
