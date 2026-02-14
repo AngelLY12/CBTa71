96ac83b2e989467bf27a86b52eaf9d431148f06f
Angel <lopezyanezangell@gmail.com>
2026-02-13 18:49:49 -0600

    fix: Fix initials

--- backend/school-management/app/Core/Domain/Utils/Helpers/Folio.php
+++ backend/school-management/app/Core/Domain/Utils/Helpers/Folio.php
@@ -10,11 +10,10 @@ class Folio
 
     public static function generateReceiptFolio(Payment $payment): string
     {
-        $conceptCode = Str::of($payment->concept_name)
-            ->initials()
-            ->upper()
-            ->substr(0, 3);
-        $conceptCode = str_pad($conceptCode, 3, 'X', STR_PAD_RIGHT);
+        $conceptCode =  collect(explode(' ', $payment->concept_name))
+            ->map(fn($word) => strtoupper(substr($word, 0, 1)))
+            ->implode('');
+        $conceptCode = substr(str_pad($conceptCode, 3, 'X', STR_PAD_RIGHT), 0, 3);
         $dateCode = now()->format('mY');
         $correlative = str_pad($payment->id,10,'0',STR_PAD_LEFT);
         return "REC-{$conceptCode}-{$dateCode}-{$correlative}";
