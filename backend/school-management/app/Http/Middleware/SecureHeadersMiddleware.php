c20d01c4c4d44250c25a5e1cb90ccc9098044a3d
Angel <lopezyanezangell@gmail.com>
2026-02-22 16:59:05 -0600

    fix: Fix headers

--- backend/school-management/app/Http/Middleware/SecureHeadersMiddleware.php
+++ backend/school-management/app/Http/Middleware/SecureHeadersMiddleware.php
@@ -12,30 +12,11 @@ class SecureHeadersMiddleware
 
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-Frame-Options', 'DENY');
-        $response->headers->set('X-XSS-Protection', '0');
-        $response->headers->set(
-            'Content-Security-Policy',
-            "default-src 'self';
-            img-src 'self' data: https:;
-            script-src 'self' 'unsafe-inline' https://js.stripe.com https://m.stripe.network;
-            style-src 'self' 'unsafe-inline';
-            frame-src https://js.stripe.com https://hooks.stripe.com https://m.stripe.network;
-            connect-src 'self' https://api.stripe.com https://m.stripe.network https://r.stripe.com https://q.stripe.com;"
-        );
-
-        $response->headers->set(
-            'Strict-Transport-Security',
-            'max-age=31536000; includeSubDomains'
-        );
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set(
             'Referrer-Policy',
             'strict-origin-when-cross-origin'
         );
-        $response->headers->set(
-            'Permissions-Policy',
-            'camera=(), microphone=(), geolocation=(), payment=(self)'
-        );
-
         return $response;
     }
 }
