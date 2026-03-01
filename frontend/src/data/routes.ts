018a54765cf6164152234bc3cb0befa875abf7ac
jhoana <yanezjhoana9@gmail.com>
2026-03-01 12:53:46 -0600

    Version 2.2.5.A

--- frontend/src/data/routes.ts
+++ frontend/src/data/routes.ts
@@ -177,6 +177,18 @@ export const routes = {
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
 </svg>`,
     }
+  },
+  exit: {
+    url: "/",
+    title: "Salir",
+    svg: {
+      filled: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path fill-rule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clip-rule="evenodd" />
+</svg>`,
+      outline: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+</svg>`,
+    }
   }
 };
 
@@ -207,6 +219,11 @@ export const routeForRole = {
       name: routes.profile.title,
       url: routes.profile.url,
       svg: routes.profile.svg
+    },
+    {
+      name: routes.exit.title,
+      url: routes.exit.url,
+      svg: routes.exit.svg
     }
   ],
   personal:
@@ -257,6 +274,11 @@ export const routeForRole = {
         url: routes.matter.url,
         svg: routes.matter.svg
       },
+      {
+        name: routes.exit.title,
+        url: routes.exit.url,
+        svg: routes.exit.svg
+      }
     ],
   aspirantes: [
     {
@@ -264,5 +286,10 @@ export const routeForRole = {
       url: routes.home.url,
       svg: routes.home.svg
     },
+     {
+      name: routes.exit.title,
+      url: routes.exit.url,
+      svg: routes.exit.svg
+    }
   ]
 };
